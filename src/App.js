08f0ccad14dc66e1199bfc72af905d3c8c1178d5
checkmeonalex <checkmeonalex@gmail.com>
2025-01-10 00:24:08 +0100

    Update: reserved 1.0 changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './components/NavBar/NavBar'; // Import NavBar component
+import './App.css'; // Import global styles
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Welcome to PACompany!</h1>
+    </div>
+  );
+}
+
+export default App;
